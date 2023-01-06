fa679ab6065ec449626c8ce3dddc58a49916e4a7
Abdoelkassas <111073150+Abdoelkassas@users.noreply.github.com>
2023-01-06 17:16:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector("#btn")
+let video = document.querySelector(".video")
+btn.onclick = function(){
+    document.write("<center><video src='vids/0105.mp4' controls autoplay width=600 height=600></video></center>")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
